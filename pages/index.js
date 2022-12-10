7b95b9864ecd40aaf72a5a3eec1e9cd4352901e2
Pavel Moiseenko <19418601+rakleed@users.noreply.github.com>
2022-12-10 22:12:26 +0300

    Fix text overflow for mobile and edit button

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,17 @@ let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 let editBtn = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
-let popupSaveBtn = document.querySelector('.popup__save-btn');
-let popupCloseBtn = document.querySelector('.popup__close-btn');
 let popupProfileName = document.querySelector('.popup__name-input');
 let popupProfileAbout = document.querySelector('.popup__about-input');
+let popupSaveBtn = document.querySelector('.popup__save-btn');
+let popupCloseBtn = document.querySelector('.popup__close-btn');
+
+function closePopupWthSave() {
+  profileName.textContent = popupProfileName.value;
+  profileAbout.textContent = popupProfileAbout.value;
+  popup.classList.remove('popup_opened');
+  popup.close();
+}
 
 editBtn.addEventListener('click', function () {
   popupProfileName.value = profileName.textContent;
@@ -14,12 +21,21 @@ editBtn.addEventListener('click', function () {
   popup.showModal();
 });
 
-popupSaveBtn.addEventListener('click', function () {
-  profileName.textContent = popupProfileName.value;
-  profileAbout.textContent = popupProfileAbout.value;
-  popup.classList.remove('popup_opened');
-  popup.close();
+popupProfileName.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    // event.preventDefault();
+    closePopupWthSave();
+  }
 });
+
+popupProfileAbout.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    // event.preventDefault();
+    closePopupWthSave();
+  }
+});
+
+popupSaveBtn.addEventListener('click', closePopupWthSave);
 
 popupCloseBtn.addEventListener('click', function () {
   popup.classList.remove('popup_opened');
